087203a61925a4f5b81c4bcb71aad669cbe4c2c4
Joshua Kifer <joshua.kifer@gmail.com>
2016-06-17 14:36:54 -0700

    Complete

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import makeStore from './src/store'
+import startServer from './src/server'
+import entries from './entries.json'
 
 export const store = makeStore()
+startServer(store)
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries,
+})
+
+store.dispatch({ type: 'NEXT' })
